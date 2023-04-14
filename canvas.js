af3d95c86c861a85fbc4541b1991b477f07c7ad0
Zach <zach.jilani1@gmail.com>
2023-04-14 13:25:30 -0700

    penguin works and moves to keys etc. however i need to change the animation rate and refresh rate to match. also currently doesnt use the Penguin class.

--- canvas.js
+++ canvas.js
@@ -1,27 +1,35 @@
 class Penguin {//may change the constructor here, im unsure.
-  constructor(width=0, height=0, x=0, y=0) {
+  constructor(width=0, height=0, x=0, y=0, image) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
-    img = new Image();
+    this.image = image;
   }
   setImg(anim, frame) {
-    img.src = 'Penguins/' + anim + '/' + String(frame) + '.png';
+    image.src = 'Penguins/' + anim + '/' + String(frame) + '.png';
   }
 }
 
 var canvas = document.querySelector('canvas');
+window.addEventListener('keydown', KeyPress, false);
+window.addEventListener('keyup', function(e){
+  anim = randomIdle();
+  console.log("key up");
+})
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 img = new Image();
+//var p = new Penguin(new Image());
 
 
 
 var context = canvas.getContext('2d');
 var x = 10;
-var xdis = 10;
+var y = 10;
+var xdis = 5;
+var ydis = 5;
 var last_animation_time = new Date().getTime();
 var time_delta = 99;
 var i = 0;
@@ -32,10 +40,8 @@ var jsondata = $.ajax({
 }).responseJSON;
 jsondata = jsondata["TenderBud"];
 
-
-Array.prototype.random = function() {
-  return this[Math.floor((Math.random()*this.length))];
-}
+var anim = randomIdle();
+console.log(anim);
 
 function randomIdle() {
   idleArr = [
@@ -52,30 +58,32 @@ function randomIdle() {
     "idleSpin",
     "idleWave"
   ]
-  return idleArr.random();
+  return idleArr[Math.floor(Math.random()*idleArr.length)];
 }
 
 function KeyPress(key) {
   switch(key.keyCode){
     //N
     case 87:
-      dir = 'walk_N';
+      anim = 'walk_N';
+      y = y - ydis;
       break;
     //E
     case 68:
-      dir = 'walk_E';
+      anim = 'walk_E';
+      x = x + xdis;
       break;
     //S
     case 83:
-      dir = 'walk_S';
+      anim = 'walk_S';
+      y = y + ydis;
       break;
     //W
     case 65:
-      dir = 'walk_W';
+      anim = 'walk_W';
+      x = x - xdis;
       break;
     default:
-    //randomidleanim
-      dir = 'none'
       break;
   }
 }
@@ -86,12 +94,13 @@ function animate() {
     return;
   }
   last_animation_time = new Date().getTime();
-  if(i > jsondata[rand].length - 1){
+  if(i > jsondata[anim].length - 1){
     i = 0;
   }
-  img.src = 'Penguins/' + rand + '/' + String(i) + '.png';
+  //p.setImg(rand, i)
+  img.src = 'Penguins/' + anim + '/' + String(i) + '.png';
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(img, x, 10, jsondata[rand][i]['w'], jsondata[rand][i]['h']);
+  context.drawImage(img, x, y, jsondata[anim][i]['w'], jsondata[anim][i]['h']);
   i++;
 }
 

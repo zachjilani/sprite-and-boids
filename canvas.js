d0e63fb69dd1ae22aa8763aa69e618b1879ad02a
Zach <zach.jilani1@gmail.com>
2023-04-14 12:24:17 -0700

    didnt want to waste more time trying to figure out imports so I just put the Penguin class inside canvas.js. Moved the switch statement for movement inside canvas.js will change dir in switch to x/y +/- and update anim

--- canvas.js
+++ canvas.js
@@ -1,21 +1,22 @@
+class Penguin {//may change the constructor here, im unsure.
+  constructor(width=0, height=0, x=0, y=0) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    img = new Image();
+  }
+  setImg(anim, frame) {
+    img.src = 'Penguins/' + anim + '/' + String(frame) + '.png';
+  }
+}
+
 var canvas = document.querySelector('canvas');
 
-
-var init_size = true;
-// window.addEventListener('resize', resizeCanvas, false);
-// function resizeCanvas() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-//   if(init_size){
-//     init_size = false;
-//     return;
-//   }
-
-// }
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 img = new Image();
-// img.src = '0.png';
+
 
 
 var context = canvas.getContext('2d');
@@ -24,25 +25,17 @@ var xdis = 10;
 var last_animation_time = new Date().getTime();
 var time_delta = 99;
 var i = 0;
-var idle = 'idle'
-
-
-
 var jsondata = $.ajax({
   url: 'animationData.json',
   async: false,
   dataType: 'json'
 }).responseJSON;
+jsondata = jsondata["TenderBud"];
+
 
 Array.prototype.random = function() {
   return this[Math.floor((Math.random()*this.length))];
 }
-
-jsondata = jsondata["TenderBud"];
-
-console.log(jsondata['idle'].length);
-var rand = randomIdle();
-console.log(rand);
 
 function randomIdle() {
   idleArr = [
@@ -62,6 +55,30 @@ function randomIdle() {
   return idleArr.random();
 }
 
+function KeyPress(key) {
+  switch(key.keyCode){
+    //N
+    case 87:
+      dir = 'walk_N';
+      break;
+    //E
+    case 68:
+      dir = 'walk_E';
+      break;
+    //S
+    case 83:
+      dir = 'walk_S';
+      break;
+    //W
+    case 65:
+      dir = 'walk_W';
+      break;
+    default:
+    //randomidleanim
+      dir = 'none'
+      break;
+  }
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -72,7 +89,6 @@ function animate() {
   if(i > jsondata[rand].length - 1){
     i = 0;
   }
-  //need to move this into Penguin class
   img.src = 'Penguins/' + rand + '/' + String(i) + '.png';
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(img, x, 10, jsondata[rand][i]['w'], jsondata[rand][i]['h']);

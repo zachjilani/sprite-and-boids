633e124874506218d016ca1295cd069c93810740
Zach <zach.jilani1@gmail.com>
2023-04-16 20:50:57 -0700

    penguin lives

--- canvas.js
+++ canvas.js
@@ -1,130 +1,91 @@
 class Penguin {
-  constructor(xPos, yPos, distance, time_delta, data) {
+  constructor(xPos, yPos, distance, time_delta) {
     this.xPos = xPos;
     this.yPos = yPos;
-    this.xDis = distance;
-    this.yDis = distance;
-    this.data = data;
+    this.distance = distance;
     this.time_delta = time_delta;
-    this.image = new Image();
+    this.animation = 'idle';
   }
-  setImg(anim, frame) {
-    this.image.src = 'Penguins/' + anim + '/' + String(frame) + '.png';
+  setAnimation(anim) {
+    this.animation = anim;
   }
-
   setPosition(x, y) {
     this.xPos = x;
     this.yPos = y;
   }
-
-  setDistance(x, y) {
-    this.xDis = x;
-    this.yDis = y;
+  setDistance(distance) {
+    this.distance = distance
   }
-
   setDelta(delta) {
     this.time_delta = delta;
   }
-
-  randomIdle() {
-    idleArr = [
-      "idle",
-      "idleBackAndForth",
-      "idleBreathing",
-      "idleFall",
-      "idleLayDown",
-      "idleLookAround",
-      "idleLookDown",
-      "idleLookLeft",
-      "idleLookUp",
-      "idleSit",
-      "idleSpin",
-      "idleWave"
-    ]
-    return idleArr[Math.floor(Math.random()*idleArr.length)];
-  }
 }//end of Penguin Class
 
+
+var img = new Image();
+var last_animation_time = new Date().getTime();
+var i = 0;
+//also tried this inside penguin class but kept running into errors.
+//i assume due to having a sync function before creating the object.
+var jsondata = $.ajax({
+  url: 'animationData.json',
+  async: false,
+  dataType: 'json'
+}).responseJSON["TenderBud"];
+const context = document.querySelector('canvas').getContext('2d');
+window.addEventListener('keydown', KeyPress, false);
+window.addEventListener('keyup', function(e){
+  p.setAnimation(randomIdle());
+})
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 var p = new Penguin(
   xPos = 50,
   yPos = 50,
   distance = 10,
-  time_delta = 80,
-  data = $.ajax({
-    url: 'animationData.json',
-    async: false,
-    dataType: 'json'
-  }).responseJSON["TenderBud"]
+  time_delta = 99
 );
-
-const canvas = document.querySelector('canvas');
-window.addEventListener('keydown', KeyPress, false);
-window.addEventListener('keyup', function(e){
-  anim = randomIdle();
-})
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-img = new Image();
-
-
-const context = canvas.getContext('2d');
-// var x = 50;
-// var y = 50;
-// var xdis = 10;
-// var ydis = 10;
-var last_animation_time = new Date().getTime();
-// var time_delta = 80;
-var i = 0;
-// var jsondata = $.ajax({
-//   url: 'animationData.json',
-//   async: false,
-//   dataType: 'json'
-// }).responseJSON["TenderBud"];
-//jsondata = jsondata["TenderBud"];
-
-//var anim = randomIdle();
-console.log(p.setImg());
-console.log(p.image);
-
-// function randomIdle() {
-//   idleArr = [
-//     "idle",
-//     "idleBackAndForth",
-//     "idleBreathing",
-//     "idleFall",
-//     "idleLayDown",
-//     "idleLookAround",
-//     "idleLookDown",
-//     "idleLookLeft",
-//     "idleLookUp",
-//     "idleSit",
-//     "idleSpin",
-//     "idleWave"
-//   ]
-//   return idleArr[Math.floor(Math.random()*idleArr.length)];
-// }
+//had this in penguin class at one point but felt like it ran slower when
+//it was in the class function.
+function randomIdle() {
+  var idleArr = [
+    "idle",
+    "idleBackAndForth",
+    "idleBreathing",
+    "idleFall",
+    "idleLayDown",
+    "idleLookAround",
+    "idleLookDown",
+    "idleLookLeft",
+    "idleLookUp",
+    "idleSit",
+    "idleSpin",
+    "idleWave"
+  ]
+  return idleArr[Math.floor(Math.random()*idleArr.length)];
+}
 
 function KeyPress(key) {
   switch(key.keyCode){
     //N
     case 87:
-      anim = 'walk_N';
-      y = y - ydis;
+      p.setAnimation('walk_N');
+      p.yPos = p.yPos - p.distance;
       break;
     //E
     case 68:
-      anim = 'walk_E';
-      x = x + xdis;
+      p.setAnimation('walk_E');
+      p.xPos = p.xPos + p.distance;
       break;
     //S
     case 83:
-      anim = 'walk_S';
-      y = y + ydis;
+      p.setAnimation('walk_S');
+      p.yPos = p.yPos + p.distance;
       break;
     //W
     case 65:
-      anim = 'walk_W';
-      x = x - xdis;
+      p.setAnimation('walk_W');
+      p.xPos = p.xPos - p.distance;
       break;
     default:
       break;
@@ -133,18 +94,16 @@ function KeyPress(key) {
 
 function animate() {
   requestAnimationFrame(animate);
-  if((time_delta+last_animation_time) > new Date().getTime()) {
+  if((p.time_delta+last_animation_time) > new Date().getTime()) {
     return;
   }
   last_animation_time = new Date().getTime();
-  if(i > jsondata[anim].length - 1){
+  if(i > jsondata[p.animation].length - 1){
     i = 0;
   }
-  //p.setImg(rand, i)
-  img.src = 'Penguins/' + anim + '/' + String(i) + '.png';
+  img.src = 'Penguins/' + p.animation + '/' + String(i) + '.png';
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(img, x, y, jsondata[anim][i]['w'], jsondata[anim][i]['h']);
+  context.drawImage(img, p.xPos, p.yPos, jsondata[p.animation][i]['w'], jsondata[p.animation][i]['h']);
   i++;
 }
-
-//animate();
+animate();

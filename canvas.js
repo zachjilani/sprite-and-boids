54b3868979d465ad66b4d903cbeecefebacd73bc
Zach <zach.jilani1@gmail.com>
2023-05-08 18:31:39 -0700

    refactored and simplified penguin class.

--- canvas.js
+++ canvas.js
@@ -1,21 +1,28 @@
 var context = document.querySelector('canvas').getContext('2d',{willReadFrequently: true}, {alpha:false});
-//window.addEventListener('keydown', KeyPress, false);
+window.addEventListener('keydown', KeyPress, false);
 window.addEventListener('keyup', function(){
-  //leader.setAnimation('idleWave');
+  p.setAnimation('idle');
 })
+
+function KeyPress(key) {
+  p.move(key);
+}
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var p = new Penguin(context, true);
+var p = new Penguin(this);
+
+
 var boids = [];
 for(let i = 0; i < 30; i++) {
   boids.push(new Boid(this))
 }
 function animate() {
   requestAnimationFrame(animate);
-  //p.draw();
   context.clearRect(0, 0, canvas.width, canvas.height);
+  p.draw();
+  p.edges();
   for(let boid of boids) {
     boid.alignment();
     boid.update();
--- penguin.js
+++ penguin.js
@@ -1,86 +1,64 @@
 class Penguin {
-  constructor(context) {
-    this.position = [50, 50];
-    this.distance = 10;
-    this.time_delta = 99;
-    this.animation = 'idleWave';
-    this.img = new Image();
+  constructor(upper) {
+    this.upper = upper;
+    this.position = new Vec(canvas.width/2, canvas.height/2);
+    this.distance = 30;
     this.index = 0;
-    this.last_animation_time = new Date().getTime();
     this.data = $.ajax({
       url: 'animationData.json',
       async: false,
       dataType: 'json'
     }).responseJSON["TenderBud"];
-    this.backgroundImage = null;
-    this.context = context;
+    this.animation = 'idle';
   }
 
-  //figure out how to get rid of this
+  draw() {
+    if(this.index > this.data[this.animation].length - 1){
+      this.index = 0;
+    }
+    var newImg = new Image();
+    newImg.src = 'Penguins/' + this.animation + '/' + String(Math.floor(this.index)) + '.png';
+    this.upper.context.drawImage(newImg, this.position.x, this.position.y, 160, 160);
+    this.index = this.index + 0.5;
+  }
+
+  edges() {
+    if(this.position.x < 0) {
+      this.position.x = canvas.width
+    }
+    if(this.position.y < 0) {
+      this.position.y = canvas.height
+    }
+    if(this.position.x > canvas.width) {
+      this.position.x = 0
+    }
+    if(this.position.y > canvas.height) {
+      this.position.y = 0
+    }
+  }
+
   setAnimation(anim) {
     this.animation = anim;
   }
 
   move(key){
     switch(key.keyCode){
-      //N
       case 87:
         this.setAnimation('walk_N');
-        this.position[1] = this.position[1] - this.distance;
+        this.position.y = this.position.y - this.distance;
         break;
-      //E
       case 68:
         this.setAnimation('walk_E');
-        this.position[0] = this.position[0] + this.distance;
+        this.position.x = this.position.x + this.distance;
         break;
-      //S
       case 83:
         this.setAnimation('walk_S');
-        this.position[1] = this.position[1] + this.distance;
+        this.position.y = this.position.y + this.distance;
         break;
-      //W
       case 65:
         this.setAnimation('walk_W');
-        this.position[0] = this.position[0] - this.distance;
-        break;
-      default:
+        this.position.x = this.position.x - this.distance;
         break;
     }
   }
-
-  draw() {
-    if((this.time_delta + this.last_animation_time) > new Date().getTime()) {
-      return;
-    }
-    this.last_animation_time = new Date().getTime();
-    if(this.backgroundImage != null) {
-      this.context.putImageData(this.backgroundImage, this.position[0], this.position[1]);
-    }
-    if(this.index > this.data[this.animation].length - 1){
-      this.index = 0;
-    }
-    this.img.src = 'Penguins/' + this.animation + '/' + String(this.index) + '.png';
-
-    //this replaces bg image incompletely.
-    this.backgroundImage = this.context.getImageData(
-      this.position[0],
-      this.position[1],
-      this.data[this.animation][this.index]['w'],
-      this.data[this.animation][this.index]['h']
-      )
-    this.index++;
-    this.context.drawImage(
-      this.img,
-      this.position[0],
-      this.position[1],
-      this.data[this.animation][this.index]['w'],
-      this.data[this.animation][this.index]['h']
-      );
-    //something is wrong here
-    if(this.position[0] < 0
-      || this.position[1] < 0
-      || this.position[0] > canvas.width
-      || this.position[1] > canvas.height
-      ) {this.position = [0,0];}
-  }
-}//end of Penguin Class
+}

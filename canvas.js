c5e2fbf0088e4769caae9f656f5d09c868988e6d
zachjilani <42560854+zachjilani@users.noreply.github.com>
2023-08-15 09:35:46 -0600

    Added comments in canvas.js

--- canvas.js
+++ canvas.js
@@ -1,4 +1,7 @@
+//getting the context
 var context = document.querySelector('canvas').getContext('2d',{willReadFrequently: true}, {alpha:false});
+
+//listener events for whenever the penguin moves and stops if the penguin is currently added
 window.addEventListener('keydown', function(key){
   this.penguinPos = p.position;
   p.move(key)
@@ -8,6 +11,7 @@ window.addEventListener('keyup', function(){
   this.penguinPos = 0;
 });
 
+//defining an original value for the position of the penguin
 this.penguinPos = 0
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -15,9 +19,13 @@ canvas.height = window.innerHeight;
 //var p = new Penguin(this);
 var boids = [];
 
+//need to have a reference to the boid array, and animate function. so Boid class takes a reference to this to use later.
+//creating array for 40 boids
 for(let i = 0; i < 40; i++) {
   boids.push(new Boid(this))
 }
+
+//animate function that draws the penguin and boids
 function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0, 0, canvas.width, canvas.height);
